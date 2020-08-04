15619cc938a6ebeb5bf570ada5f4246d4d656105
Utkarsh Sharma <46284927+utkzas@users.noreply.github.com>
2020-08-04 13:29:20 +0530

    Basic Hello World REST API

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 console.log("HI utkarsh");
+//this will be server.js
+
+//import express
+const express = require("express");
+//create an instance
+const app = express();
+//define a port
+const port=3000; 
+//define routes
+app.get("/",(req,res) => {
+console.log(0.1+0.2);
+res.json(0.1+0.2);
+});
+//listen to the port
+app.listen(port, () => {
+console.log(`Server running on port ${port}`);
+});
